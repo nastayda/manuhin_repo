fd99d2e4a037001fe9559e5937d9e2f1a11f4963
manyurij <manyurij@yandex.ru>
2016-03-14 15:29:26 +0300

    создание группы с проверкой на ID

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,46 +1,19 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
-  }
-
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            '}';
-  }
-
-  private final String id;
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
@@ -59,7 +32,40 @@ public class GroupData {
     return footer;
   }
 
-  public String getId() {
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    if (name != null ? !name.equals(groupData.name) : groupData.name != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
+  public int getId() {
     return id;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+
+
+  public void setId(int id) {
+    this.id = id;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,21 +5,28 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
-        app.getNavigationHelper().goToGroup();
-        if (! app.getGroupHelper().isThereAGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("11", "22", "33"));
-        }
-        app.getNavigationHelper().goToContacts();
-        List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().createContact(new ContactData("11", "22", "33", "44", "11.22@55", "11"), true);
-        List<ContactData> after = app.getContactHelper().getContactList();
-        Assert.assertEquals(after.size(), before.size() + 1);
+      app.getNavigationHelper().goToGroup();
+      if (! app.getGroupHelper().isThereAGroup()) {
+          app.getGroupHelper().createGroup(new GroupData("11", "22", "33"));
+      }
+      app.getNavigationHelper().goToContacts();
+      List<ContactData> before = app.getContactHelper().getContactList();
+
+      ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "11", "22", "33", "44", "11.22@55", "11");
+      app.getContactHelper().createContact(contact, true);
+      List<ContactData> after = app.getContactHelper().getContactList();
+      Assert.assertEquals(after.size(), before.size() + 1);
+
+      before.remove(before.size() - 1);
+      before.add(contact);
+      Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
 
 
