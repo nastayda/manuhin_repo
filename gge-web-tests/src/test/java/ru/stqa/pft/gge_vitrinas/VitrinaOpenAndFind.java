264331b1add4f90ffaba6b91da0a8e52a724b9d6
manyurij <manyurij@yandex.ru>
2016-03-16 09:12:55 +0300

    принудительные задержки перед кликами

--- gge-web-tests/src/test/java/ru/stqa/pft/gge_vitrinas/VitrinaOpenAndFind.java
+++ gge-web-tests/src/test/java/ru/stqa/pft/gge_vitrinas/VitrinaOpenAndFind.java
@@ -2,12 +2,15 @@ package test.java.ru.stqa.pft.gge_vitrinas;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
 
 public class VitrinaOpenAndFind {
     FirefoxDriver wd;
@@ -19,7 +22,7 @@ public class VitrinaOpenAndFind {
     }
     
     @Test
-    public void VitrinaOpenAndFind() {
+    public void testVitrinaOpenAndFind() throws Exception {
         wd.get("https://vm-082-as-gge.mdi.ru/auth/login.action");
         wd.findElement(By.id("content")).click();
         wd.findElement(By.id("username")).click();
@@ -28,19 +31,49 @@ public class VitrinaOpenAndFind {
         wd.findElement(By.id("password")).click();
         wd.findElement(By.id("password")).sendKeys("21");
         wd.findElement(By.id("submitBtn")).click();
-//        wd.findElement(By.linkText("Экспертиза")).click();
-       // wd.findElement(By.xpath("(.//*[@id='firstMenu']//a)[2]")).click();
+
+        waitElement(By.linkText("Экспертиза"));
+//        Thread.sleep(10000);
         wd.findElement(By.linkText("Экспертиза")).click();
-//        wd.findElement(By.cssSelector("div.closeBtn.btSearchStyle")).click();
+
+        waitElement(By.id("0E0CADA75DC448959686DFC63BD2178A"));
+
+//        Thread.sleep(10000);
+
         wd.findElement(By.id("0E0CADA75DC448959686DFC63BD2178A")).click();
+
+        waitElement(By.xpath("//div[@class='form']/div[2]/span/input"));
+
         wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).click();
         wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).clear();
         wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).sendKeys("ав");
+
+        waitElement(By.xpath("//div[@class='form']/input"));
+
         wd.findElement(By.xpath("//div[@class='form']/input")).click();
-        wd.findElement(By.cssSelector("div.closeBtn.btSearchStyle")).click();
+
+        Thread.sleep(10000);
+
     }
-    
-    @AfterMethod
+
+  private void waitElement(By locator) throws InterruptedException {
+    for (int second = 0;; second++) {
+      if (second >= 60) fail("timeout");
+      try { if (isElementPresent(locator)) break; } catch (Exception e) {}
+      Thread.sleep(1000);
+    }
+  }
+
+  protected boolean isElementPresent(By locator) {
+    try {
+      wd.findElement(locator);
+      return true;
+    } catch (NoSuchElementException ex) {
+      return false;
+    }
+  }
+
+  @AfterMethod
     public void tearDown() {
         wd.quit();
     }
