5dd2b1124b74db423d430682de588481235b6649
manyurij <manyurij@yandex.ru>
2016-03-17 11:24:07 +0300

    заработал тест на рабочей системе)

--- gge-web-tests/src/test/java/ru/stqa/pft/gge_vitrinas/VitrinaOpenAndFind.java
+++ gge-web-tests/src/test/java/ru/stqa/pft/gge_vitrinas/VitrinaOpenAndFind.java
@@ -13,40 +13,47 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 
 public class VitrinaOpenAndFind {
-    FirefoxDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void testVitrinaOpenAndFind() throws Exception {
-        wd.get("https://vm-082-as-gge.mdi.ru/auth/login.action");
-        wd.findElement(By.id("content")).click();
-        wd.findElement(By.id("username")).click();
-        wd.findElement(By.id("username")).clear();
-        wd.findElement(By.id("username")).sendKeys("galactica_admin1");
-        wd.findElement(By.id("password")).click();
-        wd.findElement(By.id("password")).sendKeys("21");
-        wd.findElement(By.id("submitBtn")).click();
+  FirefoxDriver wd;
 
-        waitElement(By.linkText("Экспертиза"));
-        wd.findElement(By.linkText("Экспертиза")).click();
+  @BeforeMethod
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
-        waitElement(By.id("0E0CADA75DC448959686DFC63BD2178A"));
-        wd.findElement(By.id("0E0CADA75DC448959686DFC63BD2178A")).click();
+  @Test
+  public void testVitrinaOpenAndFind() throws Exception {
+    wd.get("https://vm-082-as-gge.mdi.ru/auth/login.action");
+    wd.findElement(By.id("content")).click();
+    wd.findElement(By.id("username")).click();
+    wd.findElement(By.id("username")).clear();
+    wd.findElement(By.id("username")).sendKeys("galactica_admin1");
+    wd.findElement(By.id("password")).click();
+    wd.findElement(By.id("password")).sendKeys("21");
+    wd.findElement(By.id("submitBtn")).click();
 
-        waitElement(By.xpath("//div[@class='form']/div[2]/span/input"));
-        wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).click();
-        wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).clear();
-        wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).sendKeys("ав");
+    waitLoadPage();
+    waitElement(By.linkText("Экспертиза"));
+    wd.findElement(By.linkText("Экспертиза")).click();
 
-        waitElement(By.xpath("//div[@class='form']/input"));
-        wd.findElement(By.xpath("//div[@class='form']/input")).click();
+    waitLoadPage();
+    waitElement(By.id("0E0CADA75DC448959686DFC63BD2178A"));
+    wd.findElement(By.id("0E0CADA75DC448959686DFC63BD2178A")).click();
 
-    }
+    waitLoadPage();
+    waitElement(By.xpath("//div[@class='form']/div[2]/span/input"));
+    wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).click();
+    wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).clear();
+    wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).sendKeys("ав");
+
+    waitElement(By.xpath("//div[@class='form']/input"));
+    wd.findElement(By.xpath("//div[@class='form']/input")).click();
+
+  }
+
+  private void waitLoadPage() throws InterruptedException {
+    waitElement(By.xpath("//div[@id=\"overlay\" and @style=\"display: none;\"]"));
+  }
 
   private void waitElement(By locator) throws InterruptedException {
     Thread.sleep(3000);
@@ -56,8 +63,7 @@ public class VitrinaOpenAndFind {
       }
       try {
         if (isElementPresent(locator)) {
-          //break;
-          return;
+          break;
         }
       }
       catch (Exception e) {
@@ -76,16 +82,16 @@ public class VitrinaOpenAndFind {
   }
 
   @AfterMethod
-    public void tearDown() {
-        wd.quit();
+  public void tearDown() {
+    wd.quit();
+  }
+
+  public static boolean isAlertPresent(FirefoxDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+  }
 }
