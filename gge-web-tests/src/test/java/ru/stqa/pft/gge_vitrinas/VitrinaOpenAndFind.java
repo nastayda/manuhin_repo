d09061e2326e95ee805a55f75e3c97e8f5bb116e
manyurij <manyurij@yandex.ru>
2016-03-17 09:04:55 +0300

    11

--- gge-web-tests/src/test/java/ru/stqa/pft/gge_vitrinas/VitrinaOpenAndFind.java
+++ gge-web-tests/src/test/java/ru/stqa/pft/gge_vitrinas/VitrinaOpenAndFind.java
@@ -33,33 +33,35 @@ public class VitrinaOpenAndFind {
         wd.findElement(By.id("submitBtn")).click();
 
         waitElement(By.linkText("Экспертиза"));
-//        Thread.sleep(10000);
         wd.findElement(By.linkText("Экспертиза")).click();
 
         waitElement(By.id("0E0CADA75DC448959686DFC63BD2178A"));
-
-//        Thread.sleep(10000);
-
         wd.findElement(By.id("0E0CADA75DC448959686DFC63BD2178A")).click();
 
         waitElement(By.xpath("//div[@class='form']/div[2]/span/input"));
-
         wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).click();
         wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).clear();
         wd.findElement(By.xpath("//div[@class='form']/div[2]/span/input")).sendKeys("ав");
 
         waitElement(By.xpath("//div[@class='form']/input"));
-
         wd.findElement(By.xpath("//div[@class='form']/input")).click();
-
-        Thread.sleep(10000);
 
     }
 
   private void waitElement(By locator) throws InterruptedException {
+    Thread.sleep(3000);
     for (int second = 0;; second++) {
-      if (second >= 60) fail("timeout");
-      try { if (isElementPresent(locator)) break; } catch (Exception e) {}
+      if (second >= 60) {
+        fail("timeout");
+      }
+      try {
+        if (isElementPresent(locator)) {
+          //break;
+          return;
+        }
+      }
+      catch (Exception e) {
+      }
       Thread.sleep(1000);
     }
   }
