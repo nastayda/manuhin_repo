e5f6225e4dd5f03f449f73af511d3e33a04d905b
manyurij <manyurij@yandex.ru>
2016-03-21 21:31:20 +0300

    почистил импорты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
