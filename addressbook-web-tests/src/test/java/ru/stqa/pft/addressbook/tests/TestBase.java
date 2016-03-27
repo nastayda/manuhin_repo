f8dcf62ed1b2a6752701ab425c3426b7060c068d
manyurij <manyurij@yandex.ru>
2016-03-27 17:29:22 +0300

    логирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,24 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by Юрий on 04.03.2016.
  */
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app =
           new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX)) ;
@@ -18,12 +28,22 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
     public void tearDown() {
       app.stop();
   }
 
   public ApplicationManager getApp() {
     return app;
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m, Object[] p) {
+    logger.info("Stop test " + m.getName() + " with parameters " + Arrays.asList(p));
   }
 }
