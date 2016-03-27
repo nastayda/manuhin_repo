f220ddc3159c75c52d4492e6715903cd88723623
manyurij <manyurij@yandex.ru>
2016-03-27 12:40:11 +0300

    описание тестового стенда вынесено в конфигурационные файлы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -10,9 +10,8 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
-  //protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
-  //protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app =
+          new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX)) ;
 
   @BeforeSuite
   public void setUp() throws Exception {
