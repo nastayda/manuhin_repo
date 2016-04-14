f85ffda4691c9f7c97a4b027dac0c4884bbdde3d
manyurij <manyurij@yandex.ru>
2016-04-14 06:34:35 +0300

    разобрался: дело было в индексах

--- rzd-tests/src/test/java/ru/stqa/pft/rzd/appmanager/TicketHelper.java
+++ rzd-tests/src/test/java/ru/stqa/pft/rzd/appmanager/TicketHelper.java
@@ -21,10 +21,10 @@ public class TicketHelper extends HelperBase {
   }
 
   public void chooseDirection() throws InterruptedException {
-    type(By.id("name0"), "Москва");
+    type(By.id("name0"), "Сан");
     click(By.cssSelector("div.station"));
     Thread.sleep(10000);
-    type(By.id("name1"), "Сан");
+    type(By.id("name1"), "Москва");
     click(By.xpath("//div[7]/div[1]"));
     Thread.sleep(10000);
   }
@@ -47,36 +47,36 @@ public class TicketHelper extends HelperBase {
     Trains trains = new Trains();
     List<WebElement> rows = wd.findElements(By.xpath("//*[@class=\"trlist__trlist-row trslot \"]"));
     for (WebElement row: rows) {
-     // List<WebElement> cells = row.findElements(By.tagName("td"));
-      String number = //cells.get(3)
-              row.findElement(By.xpath("//*[@class=\"trlist__cell-pointdata__tr-num train-num-0\"]")).getText();
+      List<WebElement> cells = row.findElements(By.tagName("td"));
+      String number = cells.get(2)
+              .findElement(By.xpath(".//*[@class=\"trlist__cell-pointdata__tr-num train-num-0\"]")).getText();
       // наименование поезда
-      String name = //cells.get(3).
-              row.findElement(By.xpath("//*[@class=\"trlist__cell-pointdata__tr-brand\"]")).getText();
+      String name = cells.get(2)
+              .findElement(By.xpath(".//*[@class=\"trlist__cell-pointdata__tr-brand\"]")).getText();
       // станция отправления
-      String stationFrom = //cells.get(4).
-              row.findElement(By.xpath("(//*[@class=\"trlist__cell-pointdata__station-name\"])[1]")).getText();
+      String stationFrom = cells.get(3)
+              .findElement(By.xpath(".//*[@class=\"trlist__cell-pointdata__station-name\"]")).getText();
       // дата отправления
-      String dateFrom = //cells.get(4).
-              row.findElement(By.xpath("(//*[@class=\"trlist__cell-pointdata__date-sub\"])[1]")).getText();
+      String dateFrom = cells.get(3)
+              .findElement(By.xpath(".//*[@class=\"trlist__cell-pointdata__date-sub\"]")).getText();
       // время отправления
-      String timeFrom = //cells.get(4).
-              row.findElement(By.xpath("(//*[@class=\"trlist__cell-pointdata__time\"])[1]")).getText();
+      String timeFrom = cells.get(3)
+              .findElement(By.xpath(".//*[@class=\"trlist__cell-pointdata__time\"]")).getText();
       // станция назначения
-      String stationTo = //cells.get(8).
-              row.findElement(By.xpath("(//*[@class=\"trlist__cell-pointdata__station-name\"])[2]")).getText();
+      String stationTo = cells.get(7)
+              .findElement(By.xpath(".//*[@class=\"trlist__cell-pointdata__station-name\"]")).getText();
       // дата прибытия
-      String dateTo = //cells.get(8).
-              row.findElement(By.xpath("(//*[@class=\"trlist__cell-pointdata__date-sub\"])[2]")).getText();
+      String dateTo = cells.get(7)
+              .findElement(By.xpath(".//*[@class=\"trlist__cell-pointdata__date-sub\"]")).getText();
       // время прибытия
-      String timeTo = //cells.get(8).
-              row.findElement(By.xpath("(//*[@class=\"trlist__cell-pointdata__time\"])[2]")).getText();
+      String timeTo = cells.get(7)
+              .findElement(By.xpath(".//*[@class=\"trlist__cell-pointdata__time\"]")).getText();
       // тип вагона (сидячий, купе, св, плацкарт...)
-      String typePlace = row.findElement(By.xpath("//*[@class=\"trlist__table-price\"]/tbody/tr/td[1]")).getText();
+      String typePlace = cells.get(9).getText();
       // Кол-во мест нужного типа
-      String countPlace = row.findElement(By.xpath("//*[@class=\"trlist__table-price\"]//*[@class=\"trlist__table-price__freeseats\"]")).getText();
+      String countPlace = cells.get(10).getText();
       // Цена билета
-      String price = row.findElement(By.xpath("//*[@class=\"trlist__table-price\"]//*[@class=\"trlist__table-price__price\"]")).getText();
+      String price = cells.get(11).getText();
 
     //  int id = Integer.parseInt(row.findElement(By.tagName("input")).getAttribute("value"));
 
