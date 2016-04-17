4bf8e9435b8f72eed10539f8fb12363c33c8f458
manyurij <manyurij@yandex.ru>
2016-04-17 21:54:33 +0300

    Bugify, создание баг-репорта работает

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -1,8 +1,16 @@
 package ru.stqa.pft.rest;
 
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
@@ -13,9 +21,9 @@ import static org.testng.Assert.assertEquals;
 public class RestTests {
 
   @Test
-  public void testCreateIssue() {
+  public void testCreateIssue() throws IOException {
     Set<Issue> oldIssues = getIssue();
-    Issue newIssue = new Issue();
+    Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
     int issueId = createIssue(newIssue);
     Set<Issue> newIssues = getIssue();
     oldIssues.add(newIssue.withId(issueId));
@@ -23,11 +31,24 @@ public class RestTests {
 
   }
 
-  private Set<Issue> getIssue() {
-    Request
+  private Set<Issue> getIssue() throws IOException {
+    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
   }
 
-  private int createIssue(Issue newIssue) {
-    return 0;
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
+  }
+
+  private int createIssue(Issue newIssue) throws IOException {
+    String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
+            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                      new BasicNameValuePair("description", newIssue.getDescription())))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();
   }
 }
