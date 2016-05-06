7d0b2ae77441929191c1a165513a29f0a7604b19
manyurij <manyurij@yandex.ru>
2016-05-06 23:59:01 +0300

    допроверка на боевом

--- gge-tests/src/test/java/ru/stqa/pft/gge/tests/VitrinaOpenAndFindTests.java
+++ gge-tests/src/test/java/ru/stqa/pft/gge/tests/VitrinaOpenAndFindTests.java
@@ -38,7 +38,7 @@ public class VitrinaOpenAndFindTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromJson() throws IOException {
-    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/vitrinas_manulov_eis_all.json")))) {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/vitrinas_manulov_eis_all2.json")))) {
       String json = "";
       String line = reader.readLine();
       while (line != null) {
