e0aab220ca9275c784c9b5ee94a6191f8efa8c63
manyurij <manyurij@yandex.ru>
2016-05-14 19:13:33 +0300

    тесты по запуску браузера (в том числе удаленно)

---
+++ gge-tests/src/test/java/ru/stqa/pft/gge/tests/SeleniumSamples.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.gge.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by manuhin on 14.05.2016.
+ */
+public class SeleniumSamples {
+
+  @Test
+  public void onLocalHost() {
+    WebDriver wd = new FirefoxDriver();
+    wd.get("http://selenium2.ru");
+    wd.quit();
+  }
+
+  @Test
+  public void inRemoteOnLocalHost() {
+    WebDriver wd = new RemoteWebDriver(DesiredCapabilities.firefox());
+    wd.get("http://selenium2.ru");
+    wd.quit();
+  }
+
+  @Test
+  public void inRemoteOnRemoteHost() throws MalformedURLException {
+    WebDriver wd = new RemoteWebDriver(new URL("http://192.168.4.208:4444/wd/hub"), DesiredCapabilities.firefox());
+    wd.get("http://selenium2.ru");
+    wd.quit();
+  }
+}
