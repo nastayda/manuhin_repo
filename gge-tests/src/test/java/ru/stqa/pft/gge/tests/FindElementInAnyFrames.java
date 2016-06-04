5886e67f94501624e90d1f8146bb6042d10426e0
manyurij <manyurij@yandex.ru>
2016-06-05 00:00:42 +0300

    в iFrame-ах тоже ищет, осталось это все объединить написать метод общий

--- gge-tests/src/test/java/ru/stqa/pft/gge/tests/FindElementInAnyFrames.java
+++ gge-tests/src/test/java/ru/stqa/pft/gge/tests/FindElementInAnyFrames.java
@@ -51,7 +51,11 @@ public class FindElementInAnyFrames {
       wd = new InternetExplorerDriver(service,capabilities);
     }
 
-    wd.get("file:///C:/Users/%D0%AE%D1%80%D0%B8%D0%B9/Downloads/____%D0%9E%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5/%D0%92%D1%81%D0%B5%20%D1%81%D0%B5%D0%BA%D1%80%D0%B5%D1%82%D1%8B%20%D0%B8%20%D1%82%D0%B0%D0%B9%D0%BD%D1%8B%20Selenium%202.0/Lesson_4/%D0%94%D0%975/Frames2.html");
+    // frame
+//    wd.get("file:///C:/Users/%D0%AE%D1%80%D0%B8%D0%B9/Downloads/____%D0%9E%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5/%D0%92%D1%81%D0%B5%20%D1%81%D0%B5%D0%BA%D1%80%D0%B5%D1%82%D1%8B%20%D0%B8%20%D1%82%D0%B0%D0%B9%D0%BD%D1%8B%20Selenium%202.0/Lesson_4/%D0%94%D0%975/Frames2.html");
+
+    // iframe
+    wd.get("file:///C:/Users/%D0%AE%D1%80%D0%B8%D0%B9/Downloads/____%D0%9E%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5/%D0%92%D1%81%D0%B5%20%D1%81%D0%B5%D0%BA%D1%80%D0%B5%D1%82%D1%8B%20%D0%B8%20%D1%82%D0%B0%D0%B9%D0%BD%D1%8B%20Selenium%202.0/Lesson_4/%D0%94%D0%975/iFrames.html");
     pageFrame = new PageAnyFrame();
     PageFactory.initElements(new DisplayedElementLocatorFactory(wd, 10), pageFrame);
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
@@ -59,7 +63,9 @@ public class FindElementInAnyFrames {
 
   @Test
   public void testFindElementInAnyFrames() throws InterruptedException {
-    List<WebElement> frames = pageFrame.getFrames();
+//    List<WebElement> frames = pageFrame.getFrames();
+
+    List<WebElement> frames = pageFrame.getIframes();
     int i = 0;
     for (WebElement f : frames) {
       i++;
