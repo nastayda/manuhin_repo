624d271856552bba986247c7516d300a74ce28be
manyurij <manyurij@yandex.ru>
2016-07-02 10:35:53 +0300

    теперь должны появиться скриншоты

--- gge-tests/src/test/java/ru/stqa/pft/gge/tests/VitrinaOpenAndFindTestsMGE.java
+++ gge-tests/src/test/java/ru/stqa/pft/gge/tests/VitrinaOpenAndFindTestsMGE.java
@@ -43,7 +43,7 @@ public class VitrinaOpenAndFindTestsMGE extends TestBase{
   @DataProvider
   public Iterator<Object[]> validGroupsFromJson() throws IOException {
     try (BufferedReader reader = new BufferedReader(new FileReader(
-            new File("src/test/resources/vitrinas_mge_galactica_admin1_all_expertiza_new2.json")))) {
+            new File("src/test/resources/vitrinas_mge_galactica_admin1_part_expertiza_new2.json")))) {
       String json = "";
       String line = reader.readLine();
       while (line != null) {
@@ -65,18 +65,29 @@ public class VitrinaOpenAndFindTestsMGE extends TestBase{
       password = "123456";
     }
 
+//    assertThat(app.successInit, equalTo(true));
+//    app.session().loginGGEMGE(vitrina.getLoginUser(), password, vitrina.getBaseUrl());
+//
+//    assertThat(app.vitrina().selectVitrina(vitrina, isProdServer), equalTo(true));
+//    if (app.vitrina().checkVitrinaIs(vitrina, isProdServer)) {
+//      assertThat(app.vitrina().isMistakes(isProdServer), equalTo(false));
+//      app.vitrina().vizovRasshPoiskMGE(isProdServer);
+//      app.vitrina().fillAllFilters(isProdServer);
+//      app.vitrina().buttonFind(isProdServer);
+//      assertThat(app.vitrina().isMistakes(isProdServer), equalTo(false));
+//    } else {
+//      assertThat(app.vitrina().isMistakesOtchet(isProdServer), equalTo(false));
+//    }
+
     assertThat(app.successInit, equalTo(true));
     app.session().loginGGEMGE(vitrina.getLoginUser(), password, vitrina.getBaseUrl());
 
     assertThat(app.vitrina().selectVitrina(vitrina, isProdServer), equalTo(true));
-    if (app.vitrina().checkVitrinaIs(vitrina, isProdServer)) {
-      assertThat(app.vitrina().isMistakes(isProdServer), equalTo(false));
-      app.vitrina().vizovRasshPoiskMGE(isProdServer);
-      app.vitrina().fillAllFilters(isProdServer);
-      app.vitrina().buttonFind(isProdServer);
-      assertThat(app.vitrina().isMistakes(isProdServer), equalTo(false));
-    } else {
-      assertThat(app.vitrina().isMistakesOtchet(isProdServer), equalTo(false));
-    }
+    assertThat(app.vitrina().isMistakes(isProdServer), equalTo(false));
+
+    app.vitrina().vizovRasshPoiskMGE(isProdServer);
+    app.vitrina().fillAllFilters(isProdServer);
+    app.vitrina().buttonFind(isProdServer);
+    assertThat(app.vitrina().isMistakes(isProdServer), equalTo(false));
   }
 }
