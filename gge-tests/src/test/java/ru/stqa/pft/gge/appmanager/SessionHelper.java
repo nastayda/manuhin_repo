8c7fc514837861b19fe97e83dc719e3ef8d17d59
manyurij <manyurij@yandex.ru>
2017-01-10 15:04:40 +0200

    предбой витрины до обновления

--- gge-tests/src/test/java/ru/stqa/pft/gge/appmanager/SessionHelper.java
+++ gge-tests/src/test/java/ru/stqa/pft/gge/appmanager/SessionHelper.java
@@ -13,6 +13,19 @@ public class SessionHelper extends HelperBase {
   }
 
   public void loginGGEMGE(String username, String password, String baseUrl) {
+    //подключение и отключение админа
+
+//    wd.get("https://expertiza-test.mos.ru/portal/logout.action");
+//    click(By.id("content"));
+//    type(By.id("username"), "VASILENKORN");
+//    type(By.id("password"), password);
+//    click(By.id("submitBtn"));
+//    wd.get("https://expertiza-test.mos.ru/portal/activeUsers.action?logoutUser=SADOVIN_AL"); // на разработческом
+//    //wd.get("https://expertiza-tech.mos.ru/portal/activeUsers.action?logoutUser=AdminMGE");  //на предбое
+//    wd.get("https://expertiza-test.mos.ru/portal/logout.action");
+
+    //завершение отключения админа
+
     wd.get(baseUrl);
     click(By.id("content"));
     type(By.id("username"), username);
@@ -33,7 +46,7 @@ public class SessionHelper extends HelperBase {
   public void logout() {
     //waitElement(By.id("logoutMethod"));
     //click(By.id("logoutMethod"));
-    wd.get("https://expertiza-test.mos.ru/portal/logout.action");
+    wd.get("/portal/logout.action");
   }
   public void loginUGD(String username, String password,
                        Boolean userIsChinovnik, String baseUrl,
